1cf83cbc7fb532f579d43f9ddded6679fb6423c8
Kateryna Tymoshchuk <katymoschuk@gmail.com>
2021-06-24 18:01:48 +0300

    countdown-test

---
+++ index.js
@@ -0,0 +1,49 @@
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  start() {
+    this.intervalId = setInterval(() => {
+      const today = Date.now();
+      const countdown = this.targetDate.getTime() - today;
+      const time = this.getTimeComponents(countdown);
+
+      if (countdown < 0) {
+        clearInterval(this.intervalId);
+      }
+      
+       this.updateClockface(time);
+    }, 1000)    
+  }
+
+  getTimeComponents(time) {    
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24))) ;
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  updateClockface({ days, hours, mins, secs }) {
+     const clock = document.querySelector(this.selector);
+    
+    clock.querySelector('[data-value=days]').innerHTML = days;
+    clock.querySelector('[data-value=hours]').innerHTML = hours;
+    clock.querySelector('[data-value=mins]').innerHTML = mins;
+    clock.querySelector('[data-value=secs]').innerHTML = secs;
+  }
+}
+
+const timmer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('May 16, 2022'),
+});
+
+timmer.start();
